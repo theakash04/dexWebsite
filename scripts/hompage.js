33201a8aae294f880835587c6210300925a7b044
Akash Kumar <Aakashkkr04@gmail.com>
2023-12-23 16:26:13 +0530

    added about

--- scripts/hompage.js
+++ scripts/hompage.js
@@ -1,5 +1,4 @@
-
-
+// copy function
 let copy = document.querySelector(".copy");
 
 const copied = () =>{
@@ -15,10 +14,46 @@ const copyfnc = () =>{
   copied();
 }
 
-
 copy.addEventListener("click", () =>{
   copyfnc();
 })
 
-//fix - click event not working on screen nor the touch event
 
+//About btn function
+let aboutbtn = document.querySelector(".aboutbtn");
+let homepage = document.querySelector(".homePage");
+let aboutPage = document.querySelector(".aboutPage");
+let home = document.querySelector("#homePage");
+let about = document.querySelector("#about");
+let isHomePage = true;
+let isAbout = false;
+
+
+
+about.addEventListener("click", () =>{
+  if(isAbout === false){
+    homepage.setAttribute('style', 'display: none');
+    aboutPage.setAttribute('style', 'display: block');
+    isAbout = true;
+    isHomePage = false;
+  }
+})
+
+aboutbtn.addEventListener("click", () =>{
+  if(isHomePage === true){
+    homepage.setAttribute('style', 'display: none');
+    aboutPage.setAttribute('style', 'display: block');
+   isHomePage = false;
+   isAbout = true;
+  }
+})
+
+
+home.addEventListener("click", () =>{
+  if(isHomePage === false){
+    homepage.setAttribute('style', 'display: block');
+    aboutPage.setAttribute('style', 'display: none');
+    isHomePage = true;
+    isAbout = false;
+  }
+})
---
+++ scripts/sidebar.js
@@ -0,0 +1,8 @@
+let home = document.querySelector("#homePage");
+let projects = document.querySelector("#projects");
+let about = document.querySelector("#about");
+let blog = document.querySelector("#blog");
+let contact = document.querySelector("contact");
+
+let homepage = document.querySelector("#Home");
+
