6764f1f38228e18fd9140fed432d3bcb569aa661
Akash Kumar <Aakashkkr04@gmail.com>
2023-12-24 22:02:31 +0530

    About pages added and linked with btns and home page

--- scripts/hompage.js
+++ scripts/hompage.js
@@ -21,39 +21,7 @@ copy.addEventListener("click", () =>{
 
 //About btn function
 let aboutbtn = document.querySelector(".aboutbtn");
-let homepage = document.querySelector(".homePage");
-let aboutPage = document.querySelector(".aboutPage");
-let home = document.querySelector("#homePage");
-let about = document.querySelector("#about");
-let isHomePage = true;
-let isAbout = false;
 
-
-
-about.addEventListener("click", () =>{
-  if(isAbout === false){
-    homepage.setAttribute('style', 'display: none');
-    aboutPage.setAttribute('style', 'display: block');
-    isAbout = true;
-    isHomePage = false;
-  }
-})
-
-aboutbtn.addEventListener("click", () =>{
-  if(isHomePage === true){
-    homepage.setAttribute('style', 'display: none');
-    aboutPage.setAttribute('style', 'display: block');
-   isHomePage = false;
-   isAbout = true;
-  }
-})
-
-
-home.addEventListener("click", () =>{
-  if(isHomePage === false){
-    homepage.setAttribute('style', 'display: block');
-    aboutPage.setAttribute('style', 'display: none');
-    isHomePage = true;
-    isAbout = false;
-  }
+aboutbtn.addEventListener("click", ()=>{
+  window.open("../pages/About.html", "_self");
 })
--- scripts/sidebar.js
+++
@@ -1,8 +0,0 @@
-let home = document.querySelector("#homePage");
-let projects = document.querySelector("#projects");
-let about = document.querySelector("#about");
-let blog = document.querySelector("#blog");
-let contact = document.querySelector("contact");
-
-let homepage = document.querySelector("#Home");
-
