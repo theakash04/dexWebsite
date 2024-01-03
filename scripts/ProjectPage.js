086421a77769db0613fcb71c215c477657de53fe
Akash Kumar <Aakashkkr04@gmail.com>
2024-01-03 14:58:49 +0530

    ..

--- scripts/ProjectPage.js
+++ scripts/ProjectPage.js
@@ -1,30 +1,16 @@
 const projects =  [
   {
-    imgs: "../project-images/rock-paper-scissors.png",
-    title: "Rock Paper Scissors",
-    description: "Play the classic Rock paper scissors Web Game",
-    link: "https://github.com/theakash04/stone-paper-scissor.git",
-  },
+    imgs: "../project-images/icons8-password-48.png",
+    title: "Password Generator",
+    description: "Generate strong, secure passwords effortlessly with this user-friendly web app for enhanced online security.",
+    link: "https://github.com/theakash04/Random-Pass-Genrator.git",
+  },  
 
   {
-    imgs: "../project-images/tic-tac-toe.png",
-    title: "Tic Tac Toe",
-    description: "Play the classic Tic Tac Toe two player Web Game",
-    link: "https://github.com/theakash04/ticTacToe-WebGame.git",
-  },
-
-  {
-    imgs: "../project-images/exchange.png",
-    title: "Currency Converter",
-    description: "This web app is used to Convert amounts from one currency to two others",
-    link: "https://github.com/theakash04/Currency-Converter.git",
-  },
-
-  {
-    imgs: "../project-images/cloudy.png",
-    title: "Weather Webapp",
-    description: "This web app is used to check weather temprature using city name",
-    link: "https://github.com/theakash04/Weather-Webapp.git",
+    imgs: "../project-images/js.png",
+    title: "JS Q/A Webapp",
+    description: "Practice, track progress, and enhance skills with interactive JavaScript questions.",
+    link: "https://github.com/theakash04/Js-Question-webApp.git",
   },
 
   {
@@ -35,18 +21,32 @@ const projects =  [
   },
 
   {
-    imgs: "../project-images/js.png",
-    title: "JS Q/A Webapp",
-    description: "Practice, track progress, and enhance skills with interactive JavaScript questions.",
-    link: "https://github.com/theakash04/Js-Question-webApp.git",
+    imgs: "../project-images/cloudy.png",
+    title: "Weather Webapp",
+    description: "This web app is used to check weather temprature using city name",
+    link: "https://github.com/theakash04/Weather-Webapp.git",
   },
 
   {
-    imgs: "../project-images/icons8-password-48.png",
-    title: "Password Generator",
-    description: "Generate strong, secure passwords effortlessly with this user-friendly web app for enhanced online security.",
-    link: "https://github.com/theakash04/Random-Pass-Genrator.git",
-  }
+    imgs: "../project-images/exchange.png",
+    title: "Currency Converter",
+    description: "This web app is used to Convert amounts from one currency to two others",
+    link: "https://github.com/theakash04/Currency-Converter.git",
+  },
+
+  {
+  imgs: "../project-images/tic-tac-toe.png",
+  title: "Tic Tac Toe",
+  description: "Play the classic Tic Tac Toe two player Web Game",
+  link: "https://github.com/theakash04/ticTacToe-WebGame.git",
+  },
+  
+  {
+    imgs: "../project-images/rock-paper-scissors.png",
+    title: "Rock Paper Scissors",
+    description: "Play the classic Rock paper scissors Web Game",
+    link: "https://github.com/theakash04/stone-paper-scissor.git",
+  },
 ]
 
 let projectsContainer = document.querySelector('#projects-container');
